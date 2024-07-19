809c1015fcd48ecc187f60d652b695db2baca5fd
jongbin26 <010jjj@naver.com>
2024-07-19 10:19:28 +0900

    feat: 마라톤 컴포넌트 구현

---
+++ src/components/MarathonProblemList/dummy.js
@@ -0,0 +1,44 @@
+export const dummy = [
+  {
+    problemId: 14500,
+    problemTitle: '테트로미노',
+    problemDifficulty: 12,
+    isSolved: true,
+  },
+  {
+    problemId: 5430,
+    problemTitle: 'AC',
+    problemDifficulty: 11,
+    isSolved: true,
+  },
+  {
+    problemId: 13335,
+    problemTitle: '트럭',
+    problemDifficulty: 10,
+    isSolved: false,
+  },
+  {
+    problemId: 3085,
+    problemTitle: '사탕 게임',
+    problemDifficulty: 9,
+    isSolved: true,
+  },
+  {
+    problemId: 6588,
+    problemTitle: '골드바흐의 추측',
+    problemDifficulty: 10,
+    isSolved: false,
+  },
+  {
+    problemId: 10816,
+    problemTitle: '숫자 카드 2',
+    problemDifficulty: 7,
+    isSolved: true,
+  },
+  {
+    problemId: 2110,
+    problemTitle: '공유기 설치',
+    problemDifficulty: 12,
+    isSolved: true,
+  },
+];
