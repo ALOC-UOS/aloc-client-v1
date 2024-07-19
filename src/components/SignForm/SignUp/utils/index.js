8e908414f55634d357f45e650eacdd3538cc2c0c
jongbin26 <010jjj@naver.com>
2024-07-19 18:39:42 +0900

    fix: SignUp 컴포넌트 리팩토링

---
+++ src/components/SignForm/SignUp/utils/index.js
@@ -0,0 +1,69 @@
+export const textReducer = (state, action) => {
+  switch (action.type) {
+    //set text state
+    case 'GITHUB_ID':
+      return { ...state, githubId: action.text };
+    case 'PASSWORD':
+      return { ...state, password: action.text };
+    case 'CHECKED_PASSWORD':
+      return { ...state, checkedPassword: action.text };
+    case 'NAME':
+      return { ...state, name: action.text };
+    case 'STUDENT_ID':
+      return { ...state, studentId: action.text };
+    case 'BAEKJOON_ID':
+      return { ...state, baekjoonId: action.text };
+    case 'DISCORD_ID':
+      return { ...state, discordId: action.text };
+    case 'NOTION_EMAIL':
+      return { ...state, notionEmail: action.text };
+    default:
+      throw new Error('unknown action type');
+  }
+};
+
+export const focusReducer = (state, action) => {
+  switch (action.type) {
+    //focus state
+    case 'GITHUB_ID':
+      return { ...state, githubIdFocus: !state.githubIdFocus };
+    case 'PASSWORD':
+      return { ...state, passwordFocus: !state.passwordFocus };
+    case 'CHECKED_PASSWORD':
+      return { ...state, checkedPasswordFocus: !state.checkedPasswordFocus };
+    case 'NAME':
+      return { ...state, nameFocus: !state.nameFocus };
+    case 'STUDENT_ID':
+      return { ...state, studentIdFocus: !state.studentIdFocus };
+    case 'BAEKJOON_ID':
+      return { ...state, baekjoonIdFocus: !state.baekjoonIdFocus };
+    case 'DISCORD_ID':
+      return { ...state, discordIdFocus: !state.discordIdFocus };
+    case 'NOTION_EMAIL':
+      return { ...state, notionEmailFocus: !state.notionEmailFocus };
+    default:
+      throw new Error('unknown action type');
+  }
+};
+
+export const resetInput = (type, dispatchText, dispatchFocus) => {
+  dispatchText({ type: type, text: '' });
+  dispatchFocus({ type: type });
+};
+
+export function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+export const checkForm = (inputObject, dispatchText, dispatchFocus) => {
+  let checkBool = true;
+  Object.entries(inputObject).map(([key, value]) => {
+    if (value.check()) {
+      value.ref.current.placeholder = value.error;
+      resetInput(key, dispatchText, dispatchFocus);
+      checkBool = false;
+    }
+  });
+  return checkBool;
+};
