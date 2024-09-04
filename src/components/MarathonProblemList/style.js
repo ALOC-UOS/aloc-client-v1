6da7c6905e7691df88e1b4ec3e894c7c2c9a29a6
jongbin26 <010jjj@naver.com>
2024-09-04 10:19:31 +0900

    feat: weekly에서 daily로 변경에 따른 자물쇠 컴포넌트 추가

--- src/components/MarathonProblemList/style.js
+++ src/components/MarathonProblemList/style.js
@@ -68,6 +68,7 @@ const ProblemItem = styled.div`
       transform: scale(1.2);
     }
   }
+  ${props => props.disabled && `pointer-events:none; cursor:none;`}
 `;
 const HorizontalLine = styled.hr`
   animation: ${AppearHorizontalLine} 3s ease forwards;
