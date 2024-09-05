3f41e00a7e1f07cef731c43f45cf9a9f286c8084
baebae02 <bae4614@gmail.com>
2024-09-06 01:40:44 +0900

    ⚡️ Connect websocket and makke chat service

--- src/views/Study/index.js
+++ src/views/Study/index.js
@@ -3,22 +3,9 @@ import { useState } from 'react';
 import StudyMember from './components/StudyMember';
 import StudyChat from './components/StudyChat';
 import Button from '../../components/Buttons';
-import io from 'socket.io-client';
 
 const Study = () => {
   const [isReadyToStudy, setIsReadyToStudy] = useState(true);
-
-  //socket
-  // const [data, setData] = useState('');
-  // const socket = io.connect('wss://www.iflab.run/ws/chat');
-  // console.log(socket);
-  // socket.on('connection', socket => {
-  //   socket.on('send_message', data => {
-  //     console.log(data);
-  //     setData(data);
-  //     socket.broadcast.emit('receive_message', data);
-  //   });
-  // });
 
   const studyStartButtonProps = {
     color: 'gradationBlue',
