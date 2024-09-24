a4b6db145e550e8abdf7cdf59f7139cb3f2f81d5
subin <200516bb@gmail.com>
2024-09-24 21:31:49 +0900

    refactor: HStack 변경 및 Span 이름 변경

--- src/components/Message/CoinMessage/index.js
+++ src/components/Message/CoinMessage/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import S from './style';
+import { HStack } from '../../../styles/Stack.styles';
 
 const CoinComponent = ({ userCoin, obtainCoin, triggerAnimation }) => {
   const [coin, setCoin] = useState(userCoin);
@@ -20,16 +21,24 @@ const CoinComponent = ({ userCoin, obtainCoin, triggerAnimation }) => {
   }, [triggerAnimation]);
 
   return (
-    <S.CoinContainer>
-      <S.CoinSpan isVisible={isVisible}>{coin}코인 </S.CoinSpan>
-      <S.PlusCoinSpan
+    <HStack
+      styled={{
+        position: 'relative',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        transition: 'all 1s ease-in-out',
+      }}
+    >
+      <S.CurrentCoin isVisible={isVisible}>{coin}코인 </S.CurrentCoin>
+      <S.EarnedCoin
         ref={plusCoinSpanRef}
         isVisible={isVisible}
         width={isVisible ? plusCoinSpanRef.current?.offsetWidth : 0}
       >
         +{obtainCoin}
-      </S.PlusCoinSpan>
-    </S.CoinContainer>
+      </S.EarnedCoin>
+    </HStack>
   );
 };
 
--- src/components/Message/CoinMessage/style.js
+++ src/components/Message/CoinMessage/style.js
@@ -1,22 +1,13 @@
 import styled from 'styled-components';
 
-const CoinContainer = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
+const CurrentCoin = styled.span`
   transition: all 1s ease-in-out;
+  color: ${props => (props.isVisible ? props.theme.yellow : props.theme.titleText)};
 `;
 
-const CoinSpan = styled.span`
-  transition: all 1s ease-in-out;
-  color: ${props => !props.isVisible && props.theme.yellow};
-`;
-
-const PlusCoinSpan = styled.span`
-  margin-left: 5px;
-  color: #ffb800;
+const EarnedCoin = styled.span`
+  margin-left: ${props => (props.isVisible ? '4px' : 0)};
+  color: ${props => props.theme.yellow};
   transform: ${props => (props.isVisible ? 'translateX(0)' : 'translateX(-100%)')};
   opacity: ${props => (props.isVisible ? 1 : 0)};
   width: ${props => (props.isVisible ? `${props.width}px` : 0)};
@@ -26,4 +17,4 @@ const PlusCoinSpan = styled.span`
     transform 0.8s ease-in-out;
 `;
 
-export default { CoinContainer, CoinSpan, PlusCoinSpan };
+export default { CurrentCoin, EarnedCoin };
