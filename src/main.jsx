cad3998dff5ef45c85602d6950d1f7449204d9cc
Dodolist <dodolist3@gmail.com>
2025-02-13 00:03:16 +0900

    refactor: root render 코드 변경

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,6 @@ import './index.css'; // 이 파일이 없으면 삭제 가능
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
