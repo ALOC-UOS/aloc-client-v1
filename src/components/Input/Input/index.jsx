b8b85bca825a9a71172fa309985ab66794a5eb73
Dodolist <dodolist3@gmail.com>
2025-02-19 10:30:39 +0900

    fix: named export로 인한 변경 내용 반영

--- src/components/Input/Input/index.jsx
+++ src/components/Input/Input/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useImperativeHandle, useRef, useState } from 'react';
-import S from './style';
+import { InputBox } from './style';
 const Input = React.forwardRef(({ initialPlaceholder, isFocused, type }, ref) => {
   const [text, setText] = useState('');
   const [placeholder, setPlaceholder] = useState(initialPlaceholder);
@@ -27,7 +27,7 @@ const Input = React.forwardRef(({ initialPlaceholder, isFocused, type }, ref) =>
   }, [isFocused]);
 
   return (
-    <S.InputBox
+    <InputBox
       type={type}
       ref={inputRef}
       value={text}
