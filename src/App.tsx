9891c0356d496ea47f86aece211dea96ec6ac5cf
Dodolist <dodolist3@gmail.com>
2025-03-09 00:57:08 +0900

    refactor: problem 페이지 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '@/views/Main';
-import Problem from '@/views/Problem';
 import Member from '@/views/Member';
 import Shop from '@/views/Shop';
-import Login from '@/views/Login';
 import { serverAPI, setupInterceptors } from '@/api/axios';
 import useLoginState from '@/hooks/useLoginState';
 import useUserState from '@/hooks/useUserState';
@@ -37,9 +35,7 @@ function App() {
       <BrowserRouter>
         <TopBar />
         <Routes>
-          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Main />} />
-          <Route path="/problem" element={<Problem />} />
           <Route path="/member" element={<Member />} />
           <Route path="/shop" element={<Shop />} />
         </Routes>
