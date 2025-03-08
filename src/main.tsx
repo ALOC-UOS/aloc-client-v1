372af2398e935241d57beb2382c41e93b07c7058
Dodolist <dodolist3@gmail.com>
2025-03-09 00:52:51 +0900

    refactor: css 중복 선언 제거

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import axios from 'axios';
 
 import App from './App';
-import './index.css';
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
